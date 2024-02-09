c248d96d9652619d850681660bc045bf0264df9b
Cal009 <153200653+Cal009@users.noreply.github.com>
2024-02-09 11:19:48 +0000

    js function working to play game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,31 @@
 const buttons = document.getElementsByClassName("control");
 const playerChoiceDisplay = document.getElementsByClassName("player_choice")
 const computerChoiceDisplay = document.getElementsByClassName("computer_choice")
-const playerImage = document.getElementByid("player_image")
+const playerImage = document.getElementById("player_image")
 const computerImage = document.getElementById("computer_image")
 const playerScore = document.getElementsByClassName("user_score span")
 const computerScore = document.getElementsByClassName("computer_score span")
 const choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
 
 for (let button of buttons) {
-    button.addEventListener("click", function() {
+    button.addEventListener("click", function () {
         let playerChoice = this.getAttribute("data-choice");
         playGame(playerChoice)
     })
 }
 
+function playGame(playerChoice) {
 
+    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice];
+
+    let computerChoice = Math.floor(Math.random() *5)
+
+    computerImage.src = `assets/images/${choices[computerChoice]}.png`
+    computerImage.alt = choices[computerChoice];
+
+    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+    updateScores(result);
+
+}
