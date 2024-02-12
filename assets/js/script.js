22cfb2dab6a122a5dcfc297af5c82ae7bec29251
Cal009 <153200653+Cal009@users.noreply.github.com>
2024-02-12 17:25:53 +0000

    gameOver function added to end the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
 //Getting references to DOM elements
 
 const buttons = document.getElementsByClassName("control");
-const playerChoiceDisplay = document.getElementsByClassName("player_choice")[0]
-const computerChoiceDisplay = document.getElementsByClassName("computer_choice")[0]
-const playerImage = document.getElementById("player_image")
-const computerImage = document.getElementById("computer_image")
-const playerScoreDisplay = document.getElementById("user_score")
-const computerScoreDisplay = document.getElementById("cpu_score")
+const playerImage = document.getElementById("player_image");
+const computerImage = document.getElementById("computer_image");
+const playerScoreDisplay = document.getElementById("user_score");
+const computerScoreDisplay = document.getElementById("cpu_score");
 const choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
 const message = document.getElementsByClassName("message");
 let userScore = 0;
@@ -17,7 +15,7 @@ for (let button of buttons) {
     button.addEventListener("click", function () {
         let playerChoice = this.getAttribute("data-choice");
         playGame(playerChoice);
-    })
+    });
 }
 
 //Function to enable player and computer choice and display correct image
@@ -26,9 +24,9 @@ function playGame(playerChoice) {
     playerImage.src = `assets/images/${choices[playerChoice]}.png`;
     playerImage.alt = choices[playerChoice];
 
-    let computerChoice = Math.floor(Math.random() *5)
+    let computerChoice = Math.floor(Math.random() *5);
 
-    computerImage.src = `assets/images/${choices[computerChoice]}.png`
+    computerImage.src = `assets/images/${choices[computerChoice]}.png`;
     computerImage.alt = choices[computerChoice];
 
     let result = checkWinner(choices[computerChoice], choices[playerChoice]);
@@ -62,7 +60,7 @@ function updateScores(result) {
         msg = "Well done... You Won!";
     } else if (result === "computer") {
         computerScore++;
-        msg = "Unlucky... The computer Won."
+        msg = "Unlucky... The computer Won.";
     }
     else {
         msg = "Its a tie.";
@@ -70,4 +68,19 @@ function updateScores(result) {
     playerScoreDisplay.textContent = userScore;
     computerScoreDisplay.textContent = computerScore;
     message[0].textContent = msg;
+    if (userScore === 10 || computerScore === 10) {
+        gameOver();
+    }
+}
+
+// Function to end the game
+function gameOver() {
+    for (let button of buttons) {
+        button.disabled = true;
+    }
+    if (userScore === 10) {
+        message[0].textContent = "Congratulations, you beat the computer! Refresh the page to start again." ;
+    } else {
+        message[0].textContent = "Game Over! The computer Wins! Refresh the page to start again.";
+    }
 }
